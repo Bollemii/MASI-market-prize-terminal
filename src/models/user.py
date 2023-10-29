b79d1beb48283a1d233decaef583e5db792e2e21
Bollemii <100069388+Bollemii@users.noreply.github.com>
2023-10-29 20:17:42 +0100

    bug: fix pre-commit

--- src/models/user.py
+++ src/models/user.py
@@ -1,24 +1,44 @@
 from prisma import Prisma
 import bcrypt
 
+
 class User:
     id: int
     email: str
     passwrord: str
     is_tenant: bool
 
-    def update(self, email: str, password: str) -> 'User':
+    def update(self, email: str, password: str) -> "User":
         with Prisma() as db:
-            return db.user.update({"where": {"id": self.id}, "data": {"email": email, "password": bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')}})
+            return db.user.update(
+                {
+                    "where": {"id": self.id},
+                    "data": {
+                        "email": email,
+                        "password": bcrypt.hashpw(
+                            password.encode("utf-8"), bcrypt.gensalt()
+                        ).decode("utf-8"),
+                    },
+                }
+            )
 
     @staticmethod
-    def connection(email: str, password: str) -> 'User':
+    def connection(email: str, password: str) -> "User":
         with Prisma() as db:
             return db.user.find_first(where={"email": email, "password": password})
-        
+
     @staticmethod
-    def register(email: str, password: str, confirm_password: str) -> 'User':
+    def register(email: str, password: str, confirm_password: str) -> "User":
         if password != confirm_password:
             raise Exception("Password and confirm password must be same")
         with Prisma() as db:
-            return db.user.create({"data": {"email": email, "password": bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')}})
+            return db.user.create(
+                {
+                    "data": {
+                        "email": email,
+                        "password": bcrypt.hashpw(
+                            password.encode("utf-8"), bcrypt.gensalt()
+                        ).decode("utf-8"),
+                    }
+                }
+            )
